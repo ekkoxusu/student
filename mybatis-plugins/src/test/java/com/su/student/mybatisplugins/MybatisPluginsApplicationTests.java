07e01e7e79455871a3af9ae8954e8f19cfa5e51a
xusu <ekkoxusu@163.com>
2020-12-03 17:20:04 +0800

    init

---
+++ mybatis-plugins/src/test/java/com/su/student/mybatisplugins/MybatisPluginsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.su.student.mybatisplugins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisPluginsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
