7722e804add0ec3ec6d3608bfe11cc48585f4df3
xusu <ekkoxusu@163.com>
2020-12-30 19:58:15 +0800

    Date类型get传输也要解析

--- mybatis-plugins/src/main/java/com/su/student/mybatisplugins/config/filter/web/GlobalControllerAdviceConfig.java
+++ mybatis-plugins/src/main/java/com/su/student/mybatisplugins/config/filter/web/GlobalControllerAdviceConfig.java
@@ -1,14 +1,18 @@
 package com.su.student.mybatisplugins.config.filter.web;
 
+import com.fasterxml.jackson.databind.util.StdDateFormat;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.InitBinder;
 
 import java.beans.PropertyEditorSupport;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
 /**
  * 处理get的日期
@@ -29,13 +33,37 @@ public class GlobalControllerAdviceConfig {
         webDataBinder.registerCustomEditor(LocalDateTime.class, new PropertyEditorSupport() {
             @Override
             public void setAsText(String text) throws IllegalArgumentException {
-                setValue(LocalDateTime.parse(text, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+                setValue(LocalDateTime.parse(text, DateTimeFormatter.ofPattern(WebMvcConfig.YYYY_MM_DD_HH_MM_SS)));
             }
         });
         webDataBinder.registerCustomEditor(LocalTime.class, new PropertyEditorSupport() {
             @Override
             public void setAsText(String text) throws IllegalArgumentException {
-                setValue(LocalTime.parse(text, DateTimeFormatter.ofPattern("HH:mm:ss")));
+                setValue(LocalTime.parse(text, DateTimeFormatter.ofPattern(WebMvcConfig.HH_MM_SS)));
+            }
+        });
+        webDataBinder.registerCustomEditor(Date.class, new PropertyEditorSupport() {
+            @Override
+            public void setAsText(String text) throws IllegalArgumentException {
+                SimpleDateFormat simpleDateFormat = new SimpleDateFormat(WebMvcConfig.YYYY_MM_DD_HH_MM_SS) {
+                    @Override
+                    public Date parse(String source) {
+                        try {
+                            return super.parse(source);//支持解析指定pattern类型。
+                        } catch (Exception e) {
+                            try {
+                                return StdDateFormat.instance.parse(source);//支持解析long类型的时间戳
+                            } catch (ParseException e1) {
+                                throw new RuntimeException("日期格式非法：" + e1.getMessage());
+                            }
+                        }
+                    }
+                };
+                try {
+                    setValue(simpleDateFormat.parse(text));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
         });
     }
